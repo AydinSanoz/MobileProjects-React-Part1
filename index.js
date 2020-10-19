24958f7b8bd7ae68f3ac56d5245553098c8aad79
E2029-Aydin <aydinsanoz2@gmail.com>
2020-10-19 15:03:33 +0300

    mynewspage added to index in components

--- index.js
+++ index.js
@@ -3,14 +3,18 @@
  */
 
 import {AppRegistry} from 'react-native';
+
+
 // import News from './src/News';
 // import Main from './src/Main';
 // import App from './src/App';
 // import Flex from './src/Flex';
 // import LoginPage from './src/LoginPage';
-// import MyNewsPage from './src/MyNewsPage';
-import ToDoApp from './src/ToDoApp';
+import MyNewsPage from './src/MyNewsPage';
+// import ToDoApp from './src/ToDoApp';
 // import Shopping from './src/Shopping'
+
+
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => ToDoApp);
+AppRegistry.registerComponent(appName, () => MyNewsPage);
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 // export * from './MyButton';
 // export * from './MyInput';
-// export * from './MyNewsCard';
-// export * from './MyNewsBanner';
+export * from './MyNewsCard';
+export * from './MyNewsBanner';
 // export * from './MyBanner';
 // export * from './Input';
 // export * from './NewsCard';
