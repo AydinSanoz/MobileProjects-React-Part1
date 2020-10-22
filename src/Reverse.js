3690d5b17319c1df1ee98eb9ae39a837fc30fe79
E2029-Aydin <aydinsanoz2@gmail.com>
2020-10-22 16:53:17 +0300

    reverse work& parameter pases though component

---
+++ src/Reverse.js
@@ -0,0 +1,43 @@
+import React,{useState} from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import ReverseInput from './components/ReverseInput'
+
+const Reverse = () =>{
+    const [reversed, setReversed] = useState('');
+    
+        function reverseString(userVal){
+            setReversed(userVal.split('').reverse())
+        }
+        function reverseWord(userVal){
+            setReversed(userVal.split(' ').reverse().join(' '))
+        }
+
+    return(
+        
+        <SafeAreaView>
+            <View>
+                <Text style = {styles.Text}> All In Reverse</Text>
+                <ReverseInput
+                    placeHolder = 'Anything you want to reverse here ...'
+                    onReverseString ={reverseString}
+                    onReverseWord = {reverseWord}
+                    reverseStringButton = 'Reverse String'
+                    reverseWordButton = 'Reverse Word' 
+                    result = {reversed}/>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Reverse ;
+
+const styles = StyleSheet.create({
+    Text:{
+        margin:10,
+        color : 'red',
+        fontSize : 30,
+        textAlign :'center',
+        fontWeight : 'bold',
+
+    }
+})
---
+++ src/components/ReverseInput.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import {View,Text, TextInput, StyleSheet, Button} from 'react-native';
+
+const ReverseInput = (props) =>{
+console.log("ReverseInput -> props", props.Reverse)
+    
+    const [userVal, setUserVal] = useState('');
+    
+    
+
+    return(
+        <View>
+            <TextInput
+                style = {styles.container}
+                placeholder = {props.placeHolder}
+                placeholderTextColor ='white'
+                onChangeText = {(val)=> setUserVal(val)}
+            >
+            </TextInput>
+            <Button
+                title = {props.reverseStringButton}
+                onPress = {()=>props.onReverseString(userVal)}
+            />
+            <Button
+                title = {props.reverseWordButton}
+                onPress = {()=>props.onReverseWord(userVal)}
+            />
+            
+            <Text style = {styles.container}>{props.result}</Text>
+        </View>
+    )
+}
+
+export default ReverseInput;
+
+const styles = StyleSheet.create({
+    container:{
+        
+        backgroundColor: '#00000090',
+        color : 'white',
+        margin: 5,
+        padding: 10,
+        borderRadius: 4,
+        
+        
+    }
+})
